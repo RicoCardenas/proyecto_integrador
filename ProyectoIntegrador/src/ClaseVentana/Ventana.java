8f0f5c64f8cc9ae0938806c061f01ac45470769c
RicoCardenas <julianalejandro132465798@gmail.com>
2024-03-24 22:23:35 -0500

    Se ajustan propiedades de la Ventana

--- ProyectoIntegrador/src/ClaseVentana/Ventana.java
+++ ProyectoIntegrador/src/ClaseVentana/Ventana.java
@@ -5,5 +5,8 @@ import javax.swing.*;
 public class Ventana extends JFrame {
     public Ventana(){
         this.setSize(500,500); // Se establece el tamaño de la ventana
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); //Se establece que el proceso se finalice al cerrar la ventana
+        this.setTitle("EcuPlot");// Se establece el titulo de la ventana
+        this.setLocationRelativeTo(null); //Se establece que la ventana estara en el centro de la pantalla
     }
 }
